6f84f379ab560ec589ba8a72c0e20aaa3843a68c
∆D <ashokdey100@gmail.com>
2024-12-16 10:46:44 +0530

    up: isolated libs

---
+++ ad-weblib-cpp.hpp
@@ -0,0 +1,6 @@
+#ifndef ad_weblib_cpp_hpp
+#define ad_weblib_cpp_hpp
+
+#include "networking/ad-network-lib.hpp"
+
+#endif
---
+++ networking/ad-network-lib.hpp
@@ -0,0 +1,6 @@
+#ifndef ad_network_lib_hpp
+#define ad_network_lib_hpp
+
+#include "socket/ad-socket-lib.hpp"
+
+#endif
